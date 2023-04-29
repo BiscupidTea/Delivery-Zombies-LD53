5fd0a4ea652e4766fea761382c16bc47f43123a9
Enzo Coletta <coletta.enzo@yahoo.com.ar>
2023-04-29 14:33:10 -0300

    add enemy movement and attack

---
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [Header("Entity")]
+    [SerializeField] private GameObject Enemy;
+    [SerializeField] private GameObject Player;
+
+    [Header("Basic Info")]
+    [SerializeField] private float damage;
+    [SerializeField] private float attackRadius;
+    [SerializeField] private float attackDelayTimer;
+
+    private float attackTimer;
+    private float distancePlayerEnemy;
+
+    private void Update()
+    {
+        detectIsNearPlayer();
+
+        attackTimer += Time.deltaTime;
+    }
+
+    private void detectIsNearPlayer()
+    {
+        distancePlayerEnemy = Vector2.Distance(Enemy.transform.position, Player.transform.position) - attackRadius;
+        if (distancePlayerEnemy <= attackRadius)
+        {
+            DealDamage();
+        }
+    }
+
+    private void DealDamage()
+    {
+        if (attackTimer >= attackDelayTimer)
+        {
+            //ingresar restar vida al player
+            Debug.Log("attack");
+            attackTimer = 0;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(Enemy.transform.position, attackRadius);
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [Header("Entity")]
+    [SerializeField] private GameObject Enemy;
+    [SerializeField] private GameObject Player;
+
+    [Header("Radius")]
+    [SerializeField] private float detectionRadius;
+    [SerializeField] private float followRadius;
+
+    [Header("Basic Info")]
+    [SerializeField] private float speed;
+    [SerializeField] private float health;
+
+    private float angle;
+    private float distancePlayerEnemy;
+    private bool isChasing;
+
+    private void Start()
+    {
+        Enemy ??= GetComponent<GameObject>();
+        isChasing = false;
+    }
+    private void Update()
+    {
+        DetectPlayer();
+
+        if (isChasing)
+        {
+            Enemy.transform.position = Vector2.MoveTowards(Enemy.transform.position, Player.transform.position, speed * Time.deltaTime);
+        }
+
+        if (distancePlayerEnemy <= followRadius)
+        {
+            Enemy.transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        }
+
+        if (health <= 0) 
+        { 
+            Destroy(Enemy);
+        }
+    }
+
+    /// <summary>
+    /// Enemy 
+    /// </summary>
+    private void DetectPlayer()
+    {
+        distancePlayerEnemy = Vector2.Distance(Enemy.transform.position, Player.transform.position);
+        Vector2 direction = Player.transform.position - Enemy.transform.position;
+        direction.Normalize();
+        angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        if (distancePlayerEnemy <= detectionRadius)
+        {
+            isChasing = true;
+        }
+
+        if (distancePlayerEnemy >= followRadius)
+        {
+            isChasing = false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        //draw followRadius
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(Enemy.transform.position, followRadius);
+
+        //draw detectionRadius
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(Enemy.transform.position, detectionRadius);
+
+        if (isChasing) 
+        {
+            Gizmos.color = Color.black;
+            Gizmos.DrawLine(Enemy.transform.position, Player.transform.position);
+        }
+
+    }
+}
