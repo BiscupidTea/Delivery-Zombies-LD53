456405d777d2adfb7ad64f2185478646b907ce5d
Enzo Coletta <coletta.enzo@yahoo.com.ar>
2023-05-01 11:13:39 -0300

    Modify and delete non usable wav

--- Assets/Scripts/Music/AudioManager.cs
+++ Assets/Scripts/Music/AudioManager.cs
@@ -4,28 +4,68 @@ using System.Collections.Generic;
 
 public class AudioManager : MonoBehaviour
 {
-    public static AudioManager instance;
+    [Header("------------- Audio Source -------------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
 
-    [SerializeField] AudioSource walkSource;
-    [SerializeField] List<AudioClip> walkClips = new List<AudioClip>(); 
-    private void Awake()
+    [Header("------------- Audio clips -------------")]
+    //music
+    public AudioClip background;
+    
+    //enemy
+    public AudioClip ZombieAttack1;
+    public AudioClip ZombieAttack2;
+    public AudioClip ZombieAttack3;
+    public AudioClip ZombieAttack4;
+
+    public AudioClip Zombiedamage1;
+    public AudioClip Zombiedamage2;
+    public AudioClip Zombiedamage3;
+    public AudioClip Zombiedamage4;
+
+    public AudioClip ZombieDeath1;
+    public AudioClip ZombieDeath2;
+    public AudioClip ZombieDeath3;
+    public AudioClip ZombieDeath4;
+
+    //miselanius
+    public AudioClip Ambiente;
+
+    //player
+    public AudioClip playerDamage;
+    public AudioClip playerDelivery;
+
+    public AudioClip playerBuy1;
+    public AudioClip playerBuy2;
+    public AudioClip playerBuy3;
+
+    public AudioClip playerWalk1;
+    public AudioClip playerWalk2;
+    public AudioClip playerWalk3;
+    public AudioClip playerWalk4;
+
+    //weapons
+    public AudioClip gunShoot1;
+    public AudioClip gunShoot2;
+    public AudioClip gunShoot3;
+    public AudioClip gunShoot4;
+    
+    public AudioClip shootgunShoot1;
+    public AudioClip shootgunShoot2;
+    public AudioClip shootgunLoad;
+
+    public AudioClip rifleShoot1;
+    public AudioClip rifleShoot2;
+    public AudioClip rifleShoot3;
+    public AudioClip rifleShoot4;
+    public AudioClip rifleShoot5;
+    public AudioClip rifleShoot6;
+    public AudioClip rifleShoot7;
+
+    private void Start()
     {
-        if (instance == null)
-        {
-            instance = this;
-
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+        musicSource.clip = background;
+        musicSource.Play();
     }
 
-    public void WalkSfx()
-    {
-        AudioClip clip = walkClips[Random.Range(0, walkClips.Count)];
-
-        walkSource.PlayOneShot(clip);
-    }
 }
