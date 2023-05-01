4f72ed44a09ba1726d175c82ffa5b9abf6bab731
Enzo Coletta <coletta.enzo@yahoo.com.ar>
2023-05-01 14:59:36 -0300

    add audio mixer and finish music

---
+++ Assets/Scripts/Music/VolumeSettings.cs
@@ -0,0 +1,46 @@
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine;
+using System;
+
+public class VolumeSettings : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider MusicSlider;
+    [SerializeField] private Slider SFXSlider;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetMusicVolume();
+            SetSFXVolume();
+        }
+    }
+    public void SetMusicVolume()
+    {
+        float volume = MusicSlider.value;
+        audioMixer.SetFloat("music", MathF.Log10(volume)*20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+
+    public void SetSFXVolume()
+    {
+        float volume = SFXSlider.value;
+        audioMixer.SetFloat("SFX", MathF.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+    }
+
+    private void LoadVolume()
+    {
+        MusicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+
+        SetMusicVolume();
+        SetSFXVolume();
+    }
+}
