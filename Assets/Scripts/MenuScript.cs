69c169a7966244c10b270715e518ee7c39424d04
Enzo Coletta <coletta.enzo@yahoo.com.ar>
2023-05-01 09:35:30 -0300

    Fix win frezee and non weapons buy

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Time.timeScale = 1.0f;
+    }
+}
